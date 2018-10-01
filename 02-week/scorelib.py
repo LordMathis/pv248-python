20e1448e4de3dfc29ce16f71e8227bb2b6f86af2
LordMathis <namesny.matus@gmail.com>
2018-10-01 22:13:16 +0200

    02 - Correct load and implement format()

--- 02-week/scorelib.py
+++ 02-week/scorelib.py
@@ -1,14 +1,48 @@
 
 class Print:
-    def __init__(self, edition, print_id, partitude):
+    def __init__(self, edition, print_id, partiture):
         self.edition = edition
         self.print_id = print_id
-        self.partitude = partitude
+        self.partiture = partiture
 
-    def format():
-        pass
+    def format(self):
 
-    def composition():
+        comp = self.composition()
+
+        composers = []
+        for author in comp.authors:
+            if author.born or author.died:
+                born = author.born if author.born else ""
+                died = author.died if author.died else ""
+                composers.append(author.name + " (" + born + "--" + died + ")")
+            else:
+                composers.append(author.name)
+        composers_str = "; ".join(composers)
+
+        editors = "; ".join(a.name for a in self.edition.authors)
+
+        print("Print Number: %s" % (str(self.print_id)))
+        print("Composers: %s" % composers_str)
+        print("Title: %s" % (comp.name if comp.name else ""))
+        print("Genre: %s" % (comp.genre if comp.genre else ""))
+        print("Key: %s" % (comp.key if comp.key else ""))
+        print("Composition Year: %s" % (comp.year if comp.year else ""))
+        print("Edition: %s" % (self.edition.name if self.edition.name else ""))
+        print("Editors: %s" % editors)
+        for i, voice in enumerate(comp.voices):
+            if voice.range:
+                print("Voice %d: %s %s" % (i+1, voice.name, voice.range))
+            else:
+                print("Voice %d: %s" % (i, voice.name))
+        print("Partiture: %s" % ("yes" if self.partiture else "no"))
+        print("Incipit: %s" % (comp.incipit if comp.incipit else ""))
+
+
+
+
+
+
+    def composition(self):
         return self.edition.composition
 
 class Edition:
--- 02-week/test.py
+++ 02-week/test.py
@@ -9,13 +9,15 @@ def get_composers(composers):
     for composer in composers:
         name = re.sub('\(.*\)', "", composer)
 
-        born, died = None
+        born, died = None, None
         match = re.search(year_regex, composer)
         if match:
             born = match.group(0)
-            died = match.group(1)
+            if len(match.groups()) > 1:
+                died = match.group(1)
 
         results.append(Person(name, born, died))
+    return results
 
 def get_year(line):
     match = re.search(year_regex, line)
@@ -26,20 +28,29 @@ def get_year(line):
 def get_editors(line):
     results = []
     editors = line.split(',')
-    names = [",".join(editors[i:i+span]) for i in range(0, len(words), 2)]
+
+    if len(editors) < 2:
+        names = editors
+    else:
+        names = []
+        for i in range(0, len(editors), 2):
+            names.append(editors[i] + ',' + editors[i+1])
+
     for name in names:
         results.append(Person(name))
 
+    return results
+
 def get_voice(line):
     match = re.search(range_regex, line)
-    range, name = None
+    voice_range, name = None, None
     if match:
-        range = match.group(0)
-        name = re.sub(range, "", line)
+        voice_range = match.group(0)
+        name = re.sub(range_str, "", line)
     else:
         name = line
 
-    return Voice(name, range)
+    return Voice(name, voice_range)
 
 
 def load(filename):
@@ -47,17 +58,29 @@ def load(filename):
     with open(filename, 'r') as ins:
         lines = []
         for line in ins:
+            line = line.strip()
             if line:
                 lines.append(line)
             else:
-                prints.append(process_print(lines))
+                new_print = process_print(lines)
+                if new_print:
+                    prints.append(new_print)
                 lines = []
+
+        new_print = process_print(lines)
+        if new_print:
+            prints.append(new_print)
+
+
     return prints
 
 def process_print(lines):
 
-    print_id, composition_name, genre, key, year, edition_name, incipit = None
-    authors, editors, voices = []
+    if not lines:
+        return None
+
+    print_id, composition_name, genre, key, year, edition_name, incipit = [None]*7
+    authors, editors, voices = [], [], []
     partiture = False
 
     for line in lines:
@@ -71,21 +94,31 @@ def process_print(lines):
 
         elif key_val[0] == 'Title':
             composition_name = key_val[1].strip()
+            if not composition_name:
+                composition_name = None
 
         elif key_val[0] == 'Genre':
             genre = key_val[1].strip()
+            if not genre:
+                genre = None
 
         elif key_val[0] == 'Key':
             key = key_val[1].strip()
+            if not key:
+                key = None
 
         elif key_val[0] == 'Composition Year':
             year = get_year(key_val[1].strip())
+            if not year:
+                year = None
 
         elif key_val[0] == 'Publication Year':
             pass
 
         elif key_val[0] == 'Edition':
             edition_name = key_val[1].strip()
+            if not edition_name:
+                edition_name = None
 
         elif key_val[0] == 'Editor':
             editors = get_editors(key_val[1].strip())
@@ -100,19 +133,21 @@ def process_print(lines):
 
         elif key_val[0] == 'Incipit':
             incipit = key_val[1].strip()
+            if not incipit:
+                incipit = None
 
     composition = Composition(composition_name, incipit, key, genre, year, voices, authors)
     edition = Edition(composition, editors, edition_name)
     return Print(edition, print_id, partiture)
 
-range = '(\S+)--(\S+)'
-range_regex = re.compile(range)
+range_str = '(\S+)--(\S+)'
+range_regex = re.compile(range_str)
 year_regex = re.compile('\d\d\d\d')
 partiture_regex = re.compile('yes')
 
 if __name__ == '__main__':
     filename = argv[1]
     prints = load(filename)
-    print(prints)
     for p in prints:
         p.format()
+        print()
