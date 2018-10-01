b0d02b2b9f34ecc04cdc86cd81524455b1dcd541
LordMathis <namesny.matus@gmail.com>
2018-10-01 11:33:24 +0200

    Initial structure for week 02

---
+++ 02-week/scorelib.py
@@ -0,0 +1,26 @@
+
+class Print:
+    def __init__():
+        pass
+
+    def format():
+        pass
+        
+    def composition():
+        pass
+
+class Edition:
+    def __init__():
+        pass
+
+class Composition:
+    def __init__():
+        pass
+
+class Voice:
+    def __init__():
+        pass
+
+class Person:
+    def __init__():
+        pass
