69197d05d556bf97990330d9f0bf6b8692db28b8
LordMathis <namesny.matus@gmail.com>
2018-10-12 19:05:51 +0200

    03/Create db schema from script

--- 03-exercise/import.py
+++ 03-exercise/import.py
@@ -1,8 +1,19 @@
 from sys import argv
 import utils
+import sqlite3
 
 if __name__ == '__main__':
     input = argv[1]
     output = argv[2]
 
-    print(utils.load(input))
+    script = ''
+    with open('scorelib.sql') as schema:
+        script = schema.read()
+
+    try:
+        conn = sqlite3.connect(output)
+        conn.executescript(script)
+
+
+    except Exception as e:
+        print(e)
