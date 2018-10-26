f3e136c80f1ff471d18d38697e7af5a6c3a04d1a
LordMathis <namesny.matus@gmail.com>
2018-10-26 22:11:48 +0200

    05/First working solution

--- 05-exercise/eqn.py
+++ 05-exercise/eqn.py
@@ -1,5 +1,5 @@
 from sys import argv
-import numpy
+import numpy as np
 from numpy import linalg
 
 def parse_line(line):
@@ -30,6 +30,7 @@ def parse_line(line):
 
 def to_matrix(equations, variables):
     matrix = []
+    vec = []
     for equation in equations:
         matrix_row = []
         for var in variables:
@@ -37,9 +38,37 @@ def to_matrix(equations, variables):
                 matrix_row.append(equation[var])
             else:
                 matrix_row.append(0)
-        matrix_row.append(equation['result'])
+        vec.append(equation['result'])
         matrix.append(matrix_row)
-    return numpy.array(matrix)
+
+    return np.array(matrix), np.array(vec)
+
+def frobein(matrix, vector):
+
+    n = vector.shape[0]
+    vector = np.reshape(vector, (n,1))
+
+    augmented = np.append(matrix, vector, 1)
+
+    rank_a = linalg.matrix_rank(augmented)
+    rank_b = linalg.matrix_rank(matrix)
+
+    if rank_a != rank_b:
+        return 0
+
+    if rank_b == matrix.shape[0]:
+        return 1
+
+    return rank_b - matrix.shape[0]
+
+def solve(matrix, vector, variables):
+    res = linalg.solve(matrix, vector)
+    solution = {}
+    for i in range(0, len(variables)):
+        solution[variables[i]] = res[i]
+
+    print('Solution:', solution)
+
 
 if __name__ == '__main__':
 
@@ -53,5 +82,12 @@ if __name__ == '__main__':
             eq_system.append(equation)
             variables = list(set(vars + variables))
 
-    matrix = to_matrix(eq_system, variables)
-    print(matrix)
+    matrix, vector = to_matrix(eq_system, variables)
+    num_sol = frobein(matrix, vector)
+
+    if num_sol == 0:
+        print('no solution')
+    elif num_sol == 1:
+        solve(matrix, vector, variables)
+    else:
+        print('solution space dimension:', num_sol)
