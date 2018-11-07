049c1530ad9e091830fed1b7d135045a48afb03f
LordMathis <namesny.matus@gmail.com>
2018-11-07 20:28:22 +0100

    06/Fix peaks

--- 06-exercise/peaks.py
+++ 06-exercise/peaks.py
@@ -1,25 +1,41 @@
 from sys import argv
 import numpy as np
-import wave, struct
+import wave, struct, math
+
+time = 1
 
 if __name__ == '__main__':
-    input = argv[1]
+    fname = argv[1]
 
-    wave_file = wave.open(input, 'r')
+    with wave.open(fname, 'rb') as wav:
 
-    nframes = wave_file.getnframes()
-    nchannels = wave_file.getnchannels()
+        data_size = wav.getnframes()
+        frate = wav.getframerate()
+        nchannels = wav.getnchannels()
 
-    sample_rate = wave_file.getframerate()
-    sample_width = wave_file.getsampwidth()
+        data = wav.readframes(data_size)
 
-    T = nframes / float(sample_rate)
-    read_frames = wave_file.readframes(nframes)
+    data = np.array(struct.unpack('{n}h'.format(n=data_size*nchannels), data))
 
-    wave_file.close()
-
-    data = struct.unpack("%dh" %  nchannels*nframes, read_frames)
     data_per_channel = [data[offset::nchannels] for offset in range(nchannels)]
-    data = [sum(x)/len(x) for x in zip(*data_per_channel)]
+    data_avg = [sum(e)/len(e) for e in zip(*data_per_channel)]
 
-    print(data)
+    chunk_size = time * frate
+
+    peaks = []
+    chunk_num = math.floor(data_size / chunk_size)
+    for i in range(0, chunk_num):
+
+        chunk = data_avg[i*chunk_size:i*chunk_size+chunk_size]
+        amps = np.abs(np.fft.rfft(chunk))
+        chunk_avg = np.mean(amps)
+
+        for j in range(0, len(amps)):
+            if amps[j] >= (20 * chunk_avg):
+                peaks.append(j)
+        i += chunk_size
+
+    if len(peaks) > 0:
+        print('low = {}, high = {}'.format(np.min(peaks), np.max(peaks)))
+    else:
+        print('no peaks')
