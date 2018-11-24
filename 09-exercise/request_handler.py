163c9f692b56903349373c29309e2111bb0f2f14
LordMathis <namesny.matus@gmail.com>
2018-11-24 16:34:46 +0100

    09/Working GET

--- 09-exercise/request_handler.py
+++ 09-exercise/request_handler.py
@@ -1,8 +1,9 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
-import socket.timeout
+from socket import timeout
 
 def make_forward_handler(upstream_url):
     class ForwardHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -12,23 +13,43 @@ def make_forward_handler(upstream_url):
 
         def do_GET(self):
 
+            res_json = {}
+
             req = Request('http://' + upstream_url + self.path)
             for key in self.headers:
-                print(key)
-                print(self.headers[key])
-                req.add_header(key, self.headers[key])
+                if key == 'Host' and upstream_url not in self.headers[key]:
+                    req.add_header(key, upstream_url)
+                else:
+                    req.add_header(key, self.headers[key])
 
             try:
                 res = urlopen(req, timeout=1)
             except (HTTPError, URLError) as error:
                 print(e)
-            except socket.timeout:
-                print('timeout')
+            except timeout:
+                res_json['code'] = 'timeout'
             else:
-                print(res.getcode())
-                print(res.info())
+                res_json['code'] = res.getcode()
+                res_json['headers'] = dict(res.getheaders())
+                res_data = res.read()
 
+                try:
+                    res_data_json = json.loads(res_data)
+                except:
+                    res_json['content'] = str(res_data)
+                else:
+                    res_json['json'] = res_data_json
 
+                res_content = bytes(json.dumps(res_json,
+                                               indent=4,
+                                               sort_keys=False,
+                                               ensure_ascii=False), 'utf-8')
+
+                self.send_response(200, 'OK')
+                self.send_header('Connection', 'close')
+                self.send_header('Content-Type', 'text/json; charset=utf-8')
+                self.end_headers()
+                self.wfile.write(res_content)
 
         def do_POST(self):
             print(self)
