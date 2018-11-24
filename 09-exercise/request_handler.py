b51f6e3ee15a0f6604ea842109c98dd059c0d1a3
LordMathis <namesny.matus@gmail.com>
2018-11-24 19:05:21 +0100

    09/Working POST

--- 09-exercise/request_handler.py
+++ 09-exercise/request_handler.py
@@ -52,6 +52,59 @@ def make_forward_handler(upstream_url):
                 self.wfile.write(res_content)
 
         def do_POST(self):
-            print(self)
+
+            req_content_legth = int(self.headers['Content-Length'])
+            req_body = self.rfile.read(req_content_legth)
+            res_json = {}
+
+            try:
+                req = json.loads(req_body)
+            except:
+                res_json['code'] = 'invalid json'
+            else:
+                if (req['url'] is None or
+                    (req['type'] == 'POST' and
+                     req['content'] is None)):
+                    res_json['code'] = 'invalid json'
+                else:
+                    if req['type'] == 'POST':
+                        request = Request(req['url'],
+                                          data=req['content'],
+                                          headers=req['headers'])
+                    else:
+                        request = Request(req['url'],
+                                          headers=req['headers'])
+
+                    req_timeout = int(req['timeout']) if req['timeout'] else 1
+
+                    try:
+                        res = urlopen(request, timeout=req_timeout)
+                    except (HTTPError, URLError) as error:
+                        print(e)
+                    except timeout:
+                        res_json['code'] = 'timeout'
+                    else:
+                        res_json['code'] = res.getcode()
+                        res_json['headers'] = dict(res.getheaders())
+                        res_data = res.read()
+
+                        try:
+                            res_data_json = json.loads(res_data)
+                        except:
+                            res_json['content'] = str(res_data)
+                        else:
+                            res_json['json'] = res_data_json
+
+            res_content = bytes(json.dumps(res_json,
+                                           indent=4,
+                                           sort_keys=False,
+                                           ensure_ascii=False), 'utf-8')
+
+            self.send_response(200, 'OK')
+            self.send_header('Connection', 'close')
+            self.send_header('Content-Type', 'text/json; charset=utf-8')
+            self.end_headers()
+            self.wfile.write(res_content)
+
 
     return ForwardHTTPRequestHandler
