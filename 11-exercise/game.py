71a56327c2d090b0dbecf1956192c4a7714afe91
LordMathis <namesny.matus@gmail.com>
2018-12-06 18:27:05 +0100

    11/Working tic tac toe server

---
+++ 11-exercise/game.py
@@ -0,0 +1,56 @@
+
+
+class Game():
+
+    def __init__(self, name):
+        self.board = [[0,0,0],[0,0,0],[0,0,0]]
+        self.next = 1
+        self.name = name
+        self.status = None
+
+    def status(self):
+        return self.status
+
+    def play(self, player, x, y):
+
+        if self.next == player:
+
+            if self.board[x][y] == 0:
+                self.board[x][y] = player
+                self.next = 1 if player == 2 else 2
+                if self._check_victory(x, y):
+                    self.status = player
+                elif self._check_draw():
+                    self.status = 0
+                return 'ok', 'ok'
+            else:
+                return 'bad', '{}, {} is already occupied'.format(x, y)
+
+        else:
+            return 'bad', 'It is player\'s {} turn'.format(self.next)
+
+    def _check_victory(self, x, y):
+        #check if previous move caused a win on vertical line
+        if self.board[0][y] == self.board[1][y] == self.board[2][y]:
+            return True
+
+        #check if previous move caused a win on horizontal line
+        if self.board[x][0] == self.board[x][1] == self.board[x][2]:
+            return True
+
+        #check if previous move was on the main diagonal and caused a win
+        if x == y and self.board[0][0] == self.board[1][1] == self.board[2][2]:
+            return True
+
+        #check if previous move was on the secondary diagonal and caused a win
+        if x + y == 2 and self.board[0][2] == self.board[1][1] == self.board[2][0]:
+            return True
+
+        return False
+
+    def _check_draw(self):
+        for row in self.board:
+            for cell in row:
+                if cell == 0:
+                    return False
+        return True
---
+++ 11-exercise/ttt.py
@@ -0,0 +1,91 @@
+from sys import argv
+import urllib
+from game import Game
+import json
+from socketserver import ThreadingMixIn
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+games = {}
+max_id = 0
+
+class TicTacToeHandler(BaseHTTPRequestHandler):
+
+    def __init__(self, *args, **kwargs):
+        super(TicTacToeHandler, self).__init__(*args, **kwargs)
+
+    def do_GET(self):
+
+        global games
+        global max_id
+
+        parsed_url = urllib.parse.urlparse(self.path)
+        req_params = urllib.parse.parse_qs(parsed_url.query)
+        req_path = parsed_url.path.strip('/')
+
+        res_json = {}
+
+        if req_path == 'start':
+
+            name = req_params['name'][0] if 'name' in req_params else ''
+
+            game = Game(name)
+            games[max_id] = game
+            res_json['id'] = max_id
+
+            max_id += 1
+
+        else:
+
+            if 'game' not in req_params:
+
+                self.send_error(400, 'Bad Request')
+
+            elif int(req_params['game'][0]) not in games:
+
+                res_json['status'] = 'bad'
+                res_json['message'] = 'Game with id {} does not exists'.format(req_params['game'][0])
+
+            elif req_path == 'status':
+
+                game_id = int(req_params['game'][0])
+                game = games[game_id]
+
+                if game.status is None:
+                    res_json['board'] = game.board
+                    res_json['next'] = game.next
+                else:
+                    res_json['winner'] = game.status
+
+            elif req_path == 'play':
+
+                game_id = int(req_params['game'][0])
+                player = int(req_params['player'][0])
+                x = int(req_params['x'][0])
+                y = int(req_params['y'][0])
+
+                game = games[game_id]
+                status, message = game.play(player, x, y)
+
+                res_json['status'] = status
+                res_json['message'] = message
+
+        res_content = bytes(json.dumps(res_json,
+                                       indent=4,
+                                       sort_keys=False,
+                                       ensure_ascii=False), 'utf-8')
+
+        self.send_response(200, 'OK')
+        self.send_header('Connection', 'close')
+        self.send_header('Content-Type', 'text/json; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(res_content)
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+if __name__ == '__main__':
+    port = int(argv[1])
+
+    httpd = ThreadedHTTPServer(('', port), TicTacToeHandler)
+    httpd.serve_forever()
