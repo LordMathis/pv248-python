f0e17882a0000ded5d5ad4eac5688754cfcc49df
LordMathis <namesny.matus@gmail.com>
2019-01-08 16:19:58 +0100

    08/Fix wrong slope

--- 08-exercise/student.py
+++ 08-exercise/student.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import utils
 import math
+from scipy.optimize import curve_fit
 from datetime import datetime, timedelta
 
 format = "%Y-%m-%d"
@@ -31,17 +32,17 @@ def process_student(stud_dates, stud_exs):
     cum_pts = stud_dates.cumsum()
     cum_pts = cum_pts.rename(lambda x: date_to_day(x))
 
-    x = cum_pts.index[:, np.newaxis]
+    x = cum_pts.index
     y = cum_pts.values
 
-    a, _, _, _ = np.linalg.lstsq(x, y, rcond=None)
+    slope = curve_fit(lambda x, y: x * y, x, y)[0][0]
 
-    if a[0] == 0:
+    if slope == 0:
         date_16 = 'inf'
         date_20 = 'inf'
     else:
-        day_16 = int(16 / a[0])
-        day_20 = int(20 / a[0])
+        day_16 = int(16 / slope)
+        day_20 = int(20 / slope)
 
         date_16 = date_to_string(add_days(day_16))
         date_20 = date_to_string(add_days(day_20))
@@ -51,7 +52,7 @@ def process_student(stud_dates, stud_exs):
         'median': stud_exs.median(),
         'total': stud_exs.sum(),
         'passed': stud_exs.astype(bool).sum(),
-        'regression slope': abs(a[0]),
+        'regression slope': slope,
         'date 16': date_16,
         'date 20': date_20
     }
